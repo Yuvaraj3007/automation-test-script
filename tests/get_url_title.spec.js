252b86de17ec75fccec6bd447ebf3768febfc2f3
Yuvaraj3007 <yuvarajy805@gmail.com>
2025-12-10 13:41:50 +0530

    Second  update

--- tests/get_url_title.spec.js
+++ tests/get_url_title.spec.js
@@ -1,7 +1,7 @@
 const{test,expect}=require("@playwright/test")
 
 test("verifying",async({page})=>{
-    try{
+    
    await page.goto("https://in.bookmyshow.com/explore/home/coimbatore ")
 
    const url=await page.url()
@@ -12,9 +12,8 @@ test("verifying",async({page})=>{
     console.log("title of the page is:"+title)
 
     await expect(page).toHaveURL("https://in.bookmyshow.com/explore/home/coimbatore ")
-    await expect(page).toHaveTitle("Movie Tickets, Plays, Sports, Events & Cinemas near Coimbatore - BookMyShow Coimbatore.")
-    }
-    finally{
+   // await expect(page).toHaveTitle("Movie Tickets, Plays, Sports, Events & Cinemas near Coimbatore - BookMyShow Coimbatore.") 
+    
     await page.close()
-}
+
 })
